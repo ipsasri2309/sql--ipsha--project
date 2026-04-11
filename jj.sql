c6da8567238e5faa647e65972df0b907f343e918
ipsasri2309 <ipsasri2309@gmail.com>
2026-04-11 15:39:02 +0530

    Add files via upload

---
+++ jj.sql
@@ -0,0 +1,15 @@
+create database name;
+use name;
+create table ipsha(
+id int,
+name varchar(50),
+age int
+);
+insert into ipsha(id, name,age)
+values
+(1, "sapna", 19),
+(2,"yash",21),
+(3," ayu", 14),
+(4,"yug",13),
+(5,"priya",31);
+select* from ipsha;
